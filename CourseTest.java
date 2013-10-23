e4b84676e45a36fac814250d2b6feca4708e055b
YuanSun <yuansun@cmail.carleton.ca>
2013-10-23 17:12:56 -0400

    Update CourseTest.java

--- CourseTest.java
+++ CourseTest.java
@@ -1,64 +1,90 @@
-import junit.framework.*;
 
-public class CourseTest extends TestCase {
-	
-	public void testCourse()  {
-		String id = "23", name = "test";
-		Course x = new Course(id,name);
-		String expected = id + " " + name;
-		String actual = x.getId() + " " + x.getName();
-		Assert.assertEquals(expected, actual);
-		
-	}
-	
-	public void testGetId() {
-		String id = "23", name = "test";
-		Course x = new Course(id,name);
-		Assert.assertEquals(id, x.getId());
-	}
-	
-	public void testGetName() {
-		String id = "23", name = "test";
-		Course x = new Course(id,name);
-		Assert.assertEquals(name, x.getName());
-	}
-	
-	public void testToString() {
-		String id = "23", name = "test";
-		Course x = new Course(id,name);
-		String actual = x.toString();
-		String expected = id + " " + name;
-		Assert.assertEquals(expected, actual);
-	}
-	
-	public void testEquals() {
-		String id = "23", name = "test";
-		Course x = new Course(id,name);
-		Course y = new Course("24",name);
-		Assert.assertFalse(x.equals(y));
-		Assert.assertTrue(x.equals(x));
-		
-	}
-	
-	public void testProfessor() {
-		String id = "23", name = "test";
-		Professor professor = new Professor("some","one");
-		Course x = new Course(id,name);
-		x.setProfessor(professor);
-		Assert.assertSame(professor, x.getProfessor());
-		
-	}
-	
-	public void testStudent() {
-		String id = "23", name = "test";
-		Student student = new Student("some","one");
-		Course x = new Course(id,name);
-		x.addStudent(student);
-		Assert.assertSame(x.getStudents(),student);
-		
-		x.removeStudent(student);
-		Assert.assertNull(x.getStudents());
-	}
-	
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+/**
+ * The test class CourseTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CourseTest extends TestCase
+{
+    Course course1;
+    Course course2;
+    Course course3;
+    Professor prof;
+    Student stu1;
+    /**
+     * Default constructor for test class CourseTest
+     */
+    public CourseTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        stu1 = new Student("Yuan","Sun");
+        prof = new Professor("James","Green");
+        course1 = new Course("3010","SYSC");
+        course2 = new Course("3010","SYSC");
+        course3 = new Course("2001","SYSC");
+    }
+    
+    public void testGetId()
+    {
+        assertEquals("3010",course1.getId());
+    }
+    
+    public void testGetName()
+    {
+        assertEquals("SYSC",course1.getName());
+    }
+    
+    public void testToString()
+    {
+        assertEquals("SYSC 3010",course1.toString());
+    }
+    
+    public void testEquals_falseCase()
+    {
+        assertFalse(course3.equals(course1));
+    }
+    
+    public void testEquals_nullCase()
+    {
+        assertFalse(course3.equals(null));
+    }
+    
+    public void testEquals_trueCase()
+    {
+        assertTrue(course1.equals(course2));
+    }
+    
+    public void testGetProfessor()
+    {
+        course3.setProfessor(prof);
+        assertEquals(prof,course3.getProfessor());
+    }
+    
+    public void testAddStudent()
+    {
+        course1.addStudent(stu1);
+        assertEquals(stu1,course1.getStudents()[0]);
+    }
+    
+    public void testRemoveStudent()
+    {
+        course1.removeStudent(stu1);
+        assertEquals(null,course1.getStudents()[0]);
+    }
 }
