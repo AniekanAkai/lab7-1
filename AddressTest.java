e126e4cf5a4eda2fe4bd3089f3b9d706eeb913c5
YuanSun <yuansun@cmail.carleton.ca>
2013-10-23 17:13:11 -0400

    Update AddressTest.java

--- AddressTest.java
+++ AddressTest.java
@@ -1,46 +1,67 @@
-import junit.framework.*;
 
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+/**
+ * The test class AddressTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class AddressTest extends TestCase
+{
+    Address addr1;
+    Address addr2;
+    Address addr3;
+    /**
+     * Default constructor for test class AddressTest
+     */
+    public AddressTest()
+    {
+    }
 
-public class AddressTest extends TestCase {
-	
-	public void testAddress() throws Exception {
-		String street = "123 fake st", city = "Springfield", postalCode ="j2j4j5";
-		int unit = 1;
-		
-		Address x = new Address(street,city,postalCode);
-		String expected = street + " " + city + " " + postalCode;
-		String actual = x.toString();
-		Assert.assertEquals(expected, actual);
-		
-		Address y = new Address(unit,street,city,postalCode);
-		actual = y.toString();
-		expected = Integer.toString(unit)+ " " + street + " " + city + " " + postalCode;
-		Assert.assertEquals(expected, actual);
-	}
-	
-	public void testToString() throws Exception{
-		
-		Address x = new Address("1","2","3");
-		String actual = x.toString();
-		String expected = "1 2 3";
-		Assert.assertEquals(expected, actual);
-		
-		Address y = new Address(2,"5 fake st","City","1q1q1q");
-		actual = y.toString();
-		expected = "2 5 fake st city 1q1q1q";
-		Assert.assertEquals(expected, actual);
-		
-	}
-	
-	public void testEquals() throws Exception {
-		String street = "123 fake st", city = "Springfield", postalCode ="j2j4j5";
-		int unit = 1;
-		Address x = new Address(unit,street,city,postalCode);
-		Address y = new Address(street,city,postalCode);
-		Assert.assertFalse(x.equals(y));
-		Assert.assertTrue(x.equals(x));
-	}
-	
-
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        try {
+            addr1 = new Address("1401 Prince of Wales","Ottawa","K2C3J8");
+            addr2 = new Address("1401 Prince of Wales","Ottawa","K2C3J8");
+            addr3 = new Address(507,"1401 Prince of Wales","Ottawa","K2C3J8");
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    public void testToString_firstConstructor()
+    {
+        assertEquals("1401 Prince of Wales, Ottawa, K2C3J8",addr1.toString());
+    }
+    
+    public void testToString_secondConstructor()
+    {
+        assertEquals("507, 1401 Prince of Wales, Ottawa, K2C3J8",addr3.toString());
+    }
+    
+    public void testEquals_nullCase()
+    {
+        assertFalse(addr1.equals(null));
+    }
+    
+    public void testEquals_true()
+    {
+        assertTrue(addr1.equals(addr2));
+    }
+    
+    public void testEquals_falseCase()
+    {
+        assertFalse(addr1.equals(addr3));
+    }
 }
