3452c78d9f49ae4ad48773d1d2d4471f58dbb314
YuanSun <yuansun@cmail.carleton.ca>
2013-10-23 17:12:34 -0400

    Update NameTest.java

--- NameTest.java
+++ NameTest.java
@@ -1,20 +1,64 @@
-import junit.framework.*;
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class NameTest extends TestCase {
-	
-	public void testName(){
-		String first = "test", last = "code";
-		Name x = new Name(first,last);
-		String expected = first + " " + last;
-		String actual = x.toString();
-		Assert.assertEquals(expected, actual);
-	}
+/**
+ * The test class NameTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class NameTest extends TestCase
+{
+    Name name1;
+    Name name2;
+    Name name3;
+    
+    /**
+     * Default constructor for test class NameTest
+     */
+    public NameTest()
+    {
+    }
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        name1 = new Name("Yuan","Sun");
+        name2 = new Name("Yuan","Sun");
+        name3 = new Name("John","Doe");
+    }
 
-	public void testEquals(){
-		String first = "test", last = "code";
-		Name x = new Name(first,last);
-		Name y = new Name(first,"fail");
-		Assert.assertFalse(x.equals(y));
-		Assert.assertTrue(x.equals(x));
-	}
+    @Test
+    public void testToString()
+    {
+        assertEquals("Yuan Sun",name1.toString());
+    }
+
+    public void testEquals_trueCase()
+    {
+        assertTrue(name1.equals(name2));
+    }
+    
+    public void testEquals_sameCase()
+    {
+        assertTrue(name1.equals(name1));
+    }
+    
+    public void testEquals_falseCase()
+    {
+        assertFalse(name1.equals(name3));
+    }
+    
+    public void testEquals_nullCase()
+    {
+        assertFalse(name2.equals(null));
+    }
 }
